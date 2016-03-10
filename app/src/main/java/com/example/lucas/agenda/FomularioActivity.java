3565d035f0bd157a162101a214386b3b47e2e24e
Lucas Helal <lucasghelal@gmail.com>
2016-03-10 11:34:10 -0300

    finalizando aula 2

--- app/src/main/java/com/example/lucas/agenda/FomularioActivity.java
+++ app/src/main/java/com/example/lucas/agenda/FomularioActivity.java
@@ -6,6 +6,8 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 public class FomularioActivity extends AppCompatActivity {
 
@@ -22,6 +24,14 @@ public class FomularioActivity extends AppCompatActivity {
             public void onClick(View view) {
                 Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
                         .setAction("Action", null).show();
+            }
+        });
+
+        Button botaoSalvar = (Button) findViewById(R.id.formulario_salvar);
+        botaoSalvar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(FomularioActivity.this, "Salvandoo..", Toast.LENGTH_SHORT).show();
             }
         });
     }
